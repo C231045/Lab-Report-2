d41c918aace6831fd8a5c4705c1c841b1311b654
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-16 16:05:31 +0600

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,1 +1,20 @@
+import java.util.Scanner; 
+
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer number: ");
+        num = scanner.nextInt(); // Read the user input
+
+        if (num % 2 == 0) {
+            System.out.println("The given number " + num + " is Even");
+        } else {
+            System.out.println("The given number " + num + " is Odd");
+        }
+
+        scanner.close(); 
+    }
+}
 

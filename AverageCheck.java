06f0e9f441be3504f0f513262946b2296a211c83
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-19 16:28:28 +0600

    Create AverageCheck.java

---
+++ AverageCheck.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+
+public class Averagecheck {
+   public static void main(string[] arges) {
+
+scanner sc = new Scanner(System.in);
+
+    double A = sc.nextDouble();
+double B = sc.nextDouble();
+double C = sc.nextDouble();
+
+    double average = ( A + B ) / 2;
+
+  if( average > c ) {
+ System.out.println("YES");
+} else {
+  System.out.println("NO");
+}
+
+sc.close();
+}
+]

4bc6d892f4e4fefaf74f40445e50b8a254eefbf6
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-27 20:25:11 +0600

    Create 2EvenOdd.java

---
+++ 2EvenOdd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        // Create Scanner object for input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user for input
+        System.out.print("Enter a number: ");
+        
+        // Read the number
+        int number = scanner.nextInt();
+        
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
